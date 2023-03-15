65105f6c1a4f18691976b532d6f477a8558f12ea
poldro <paolo.valmori@gmail.com>
2023-03-15 23:35:12 +0100

    testing size

--- src/containers/Grid.tsx
+++ src/containers/Grid.tsx
@@ -1,6 +1,25 @@
+import Image from "next/image";
+
 export const Grid = () => {
-    return (
-    <>
-    </>
-        )
-}
+  const emptySquare = "./svg/empty-square.svg";
+  const fullSquare = "./svg/full-square.svg";
+
+  const fullimages = Array.from({ length: 1000 }).map((_, i) => (
+    <SquareImage key={i} src={fullSquare} />
+  ));
+
+  const emptyImages = Array.from({ length: 1000 }).map((_, i) => (
+    <SquareImage key={i} src={emptySquare} />
+  ));
+
+  return(
+<>
+<div className="grid grid-cols-12 gap-1">{fullimages}</div>;
+<div className="grid grid-cols-12 gap-1">{emptyImages}</div>;
+</>
+  ) 
+};
+
+const SquareImage = ({ src }) => {
+  return <Image src={src} alt="empty square" width="12" height="12" />;
+};
